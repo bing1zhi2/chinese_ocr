9c205a995bef1fd59d8c4f83f3797c39f7b764ed
chenhao <bing1zhi2@163.com>
2019-10-22 11:18:01 +0800

    修复转换标签文件时，如果标签中有空格造成的bug

--- chinese_ocr/tools/tmp_label_to_id_label.py
+++ chinese_ocr/tools/tmp_label_to_id_label.py
@@ -47,8 +47,7 @@ def main(dataset_url, dict_file_name, tmp_label_path):
     """
     dict_file_path = os.path.join(dataset_url, dict_file_name)
 
-    out_dir = dict_file_path
-    out_file = "my_dataset_label.txt"
+    out_file_name = "my_dataset_label.txt"
 
     char_set = open(dict_file_path, 'r', encoding='utf-8').readlines()
     char_set = ''.join([ch.strip('\n') for ch in char_set][1:] + ['卍'])
@@ -57,17 +56,21 @@ def main(dataset_url, dict_file_name, tmp_label_path):
     for i, c in enumerate(char_set):
         d[c] = str(i + 1)
 
-    g = open(os.path.join(out_dir, out_file), "w", encoding="utf-8")
+    g = open(os.path.join(dataset_url, out_file_name), "w", encoding="utf-8")
     s = 0
     with open(tmp_label_path, "r", encoding="utf-8") as f:
         for line in f:
             line = line.strip()
-            label = line.split(" ")
-            words = label[1]
-            img_name = label[0]
+            # label = line.split(" ")
+            (img_name, *line_text) = line.split(" ")
+            words = " ".join(line_text)
             print(words)
             label_list = []
+            if img_name =="00003580":
+                print("dddd")
             for w in words:
+                if w == ' ':
+                    print("aaaa ", d[w])
                 label_list.append(d[w])
 
             label_str = " ".join(label_list)
@@ -90,5 +93,8 @@ def parse_arguments(argv):
 
 
 if __name__ == '__main__':
-    print("args:", sys.argv[1:])
-    main(parse_arguments(sys.argv[1:]))
+    ### F:/code/other/text_renderer/output char_7473.txt F:/code/other/text_renderer/output/default/tmp_labels.txt
+    a = sys.argv[1:]
+    print("args:", a)
+    args = parse_arguments(a)
+    main(args.dataset_url, args.dict_file_name, args.tmp_label_path)
